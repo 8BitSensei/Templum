26b2f465a7fce98eadfa5140a416c854d1bf8cba
Charlie Tizzard Ó Kevlahan <ctkevlahan@gmail.com>
2024-06-11 08:11:04 +0100

    Decap CMS changes (#13)

--- githubapi.js
+++ githubapi.js
@@ -5,6 +5,54 @@ export default class GithubAPi {
       this.branch = branch;
     }
   
+    getDirectory(name, callback){
+      fetch('https://api.github.com/repos/'+this.username+'/'+this.repo+'/contents/data/'+ name + '?ref='+this.branch)
+      .then(response => response.json())
+      .then((data) => {
+        let aggregatedData = {
+          name: name,
+          content: []
+        };
+
+        var urls = []
+        for (let i = 0; i < data.length; i++)
+        {
+          var element = data[i]
+          var rawPath = element.download_url;
+          urls.push(rawPath);
+        }
+
+        var requets = urls.map(function(url){
+          if(url.endsWith(".json"))
+          {
+            return fetch(url)
+            .then(response => response.json())
+            .then((data) => {
+              if(data) {
+                aggregatedData.content.push(data)
+              }
+            },
+            (error) => {
+              return callback(error, undefined);
+            })
+          }
+        })
+        
+
+
+        Promise.all(requets)
+        .then((results) => {
+          return callback(undefined, aggregatedData)
+        },
+        (error) => {
+          return callback(error, undefined);
+        })
+      },
+      (error) => {
+        return callback(error, undefined);
+      })
+    }
+
     get(name, callback) {
       if (name.indexOf(".json") !== -1) {
         console.log('https://raw.githubusercontent.com/'+this.username+'/'+this.repo+'/'+this.branch+'/'+name);
